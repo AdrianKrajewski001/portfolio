99c40993dbecfc8e13216093c390345741830e74
AdrianKrajewski001 <krajewski.adrian001@gmail.com>
2022-03-02 15:03:38 +0100

    HTML parser from string

--- src/js/util/stringToHTML.ts
+++ src/js/util/stringToHTML.ts
@@ -1,5 +1,5 @@
 export const stringToHTML = (htmlAsString: string): HTMLDivElement => {
-  const element = document.createElement("div");
+  const element: HTMLDivElement = document.createElement("div");
   element.innerHTML = htmlAsString;
   console.log(element);
   return element;
