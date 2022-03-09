c698c63b0a1d21362fba045e75a12f211b56a7e6
AdrianKrajewski001 <krajewski.adrian001@gmail.com>
2022-03-09 13:51:58 +0100

    Js folder structure change

--- src/js/utils/viewUtils.ts
+++ src/js/utils/viewUtils.ts
@@ -1,5 +1,3 @@
-import { changeView } from "..";
-
 export const loadHTMLFile = (fileName: string): string => {
   const { default: htmlAsString } = require(`../../html/${fileName}.html`);
   return htmlAsString;
@@ -13,15 +11,7 @@ export const stringToHTML = (htmlAsString: string): HTMLElement => {
 export const loadHTMLFromFile = (element: HTMLElement, fileName: string) => {
   element.appendChild(stringToHTML(loadHTMLFile(fileName)));
 };
-export const initializePage = () => {
-  const container: HTMLElement = document.getElementById("container");
-  loadHTMLFromFile(container, "nav");
-  loadHTMLFromFile(container, "about");
-  document.getElementById("about").classList.add("navbar-item-active");
-  document.querySelectorAll("a.navbar-item").forEach((item) => {
-    document.getElementById(item.id).onclick = () => changeView(item.id);
-  });
-};
+
 export const setActiveView = (activeView: string, view: string) => {
   document.getElementById(activeView).classList.remove("navbar-item-active");
   document.getElementById(view).classList.add("navbar-item-active");
---
+++ src/js/viewHandler.ts
@@ -0,0 +1,78 @@
+import { loadHTMLFromFile, setActiveView } from "./utils/viewUtils";
+import { formHandler } from "./formHandler";
+import Card, { Project } from "./projectCard";
+import Technology, { Technologies } from "./technologies";
+import { debounce } from "lodash";
+enum View {
+  ABOUT = "about",
+  PROJECTS = "projects",
+  TECHNOLOGIES = "technologies",
+  CONTACT = "contact",
+}
+
+const cardsArray = require("../assets/json/projects.json").map(
+  (project: Project) => {
+    return new Card(project);
+  }
+);
+const technologiesArray = require("../assets/json/technologies.json").map(
+  (technology: Technologies) => {
+    return new Technology(technology);
+  }
+);
+let activeView: string = View.ABOUT;
+
+export const initializePage = () => {
+  const container: HTMLElement = document.getElementById("container");
+  loadHTMLFromFile(container, "nav");
+  loadHTMLFromFile(container, "about");
+  document.querySelectorAll("a.navbar-item").forEach((item) => {
+    document.getElementById(item.id).onclick = () => changeView(item.id);
+  });
+};
+
+const changeViewUsingWheel = (e: WheelEvent) => {
+  const viewsArray: View[] = Object.values(View);
+  const indexOfCurrentView: number = viewsArray.findIndex(
+    (item) => item === activeView
+  );
+  if (e.deltaY > 0 && indexOfCurrentView < 3 && !e.ctrlKey) {
+    changeView(viewsArray[indexOfCurrentView + 1]);
+  } else if (e.deltaY < 0 && indexOfCurrentView > 0 && !e.ctrlKey) {
+    changeView(viewsArray[indexOfCurrentView - 1]);
+  }
+};
+
+window.addEventListener("wheel", debounce(changeViewUsingWheel, 100));
+
+const changeView = (view: string): void => {
+  if (view !== activeView) {
+    document.querySelector("main").remove();
+    activeView = setActiveView(activeView, view);
+    const container: HTMLElement = document.getElementById("container");
+    loadHTMLFromFile(container, view);
+
+    switch (view) {
+      case View.PROJECTS: {
+        cardsArray.forEach((card: Card) => {
+          document.getElementById("cards").appendChild(card.generateHTML());
+        });
+        Card.count = 0;
+        break;
+      }
+      case View.TECHNOLOGIES: {
+        technologiesArray.forEach((technology: Card) => {
+          document
+            .getElementById("technologiesContainer")
+            .appendChild(technology.generateHTML());
+        });
+        break;
+      }
+      case View.CONTACT: {
+        document
+          .getElementById("contactForm")
+          .addEventListener("submit", formHandler);
+      }
+    }
+  }
+};
